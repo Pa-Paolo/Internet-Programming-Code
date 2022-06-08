fb701e744cb4e05ec225c19f37e05177454be804
Paolo Bono <paolobono98@gmail.com>
2022-06-08 14:31:57 +0200

    Aggiunti commenti sem2 e sock client

--- ex_shmV_test_2.c
+++ ex_shmV_test_2.c
@@ -43,9 +43,10 @@ main(int argc, char **argv) {
         int *p;
         int n;
 
-        sem_t *csemR, *csemW;
+        sem_t *csemR, *csemW; //A differenza del caso precedente dichiariamo due semafori
 
         /* Child process */
+        //Siamo nel child process, per usare i semafori ci serve aprirli.
         p = getshmaddr();
         if ((csemR = sem_open(SEM1_NAME, O_CREAT, PERM_FILE, 0)) == SEM_FAILED) {
             perror("Child sem1 open");
@@ -57,15 +58,19 @@ main(int argc, char **argv) {
         }
 
         while (1) {
-            sem_wait(csemR);
+            sem_wait(csemR); //Non vado a leggere il valore di *p finchè il semaforo non è libero. Se qualcuno sta usando la
+            //zona di memoria allora sem_wait aspetta finchè il valore di semaforo non viene incrementato e poi lo decrementa lui
+            //siccome prende accesso alla zona di memoria.
             n = *p;
-            sem_post(csemW);
+            sem_post(csemW); //Sem post aumenta il valore del semaforo rendendo libero l'accesso a un eventuale processo che sta aspettando
+            //per poter accedere/scrivere la memoria. Una volta che copio il valore in "n" allora non mi importa se qualcun altro la leggerà o
+            //modificherà siccome io ho già preso il valore che mi interessava.
             if (n == 99) break;
             printf("child saw %d\n", n);
         }
 
         printf("child is done\n");
-        sem_close(csemR);
+        sem_close(csemR); //Una volta che il loop è finito chiudo l'accesso ai semafori che sono nel kernel space.
         sem_close(csemW);
         exit(0);
     } else {
@@ -86,17 +91,23 @@ main(int argc, char **argv) {
         }
 
         /* Parent process */
+        //Il procedimento è molto simile a quello fatto per il child fino a questo punto. Ora il parent vuole far si che
+        //Mentre lui scrive il valore dentro alla memoria condivisa (puntata da *p) nessun altro possa leggere o scrivere
+        //(gli altri devono comunque implementare il corrispondente semaforo, con lo stesso nome)
         p = getshmaddr();
-        *p = 0;
-        sem_post(semR);
+        *p = 0; //INIZIALIZZO IL VALORE NELLA MEMORIA CONDIVISA, IL SEMAFORO E' BLOCCANTE DI DEFAULT QUINDI SONO SICURO
+        //CHE IL CHILD NON POSSA LEGGERE PER ORA.
+        sem_post(semR); //INCREMENTO INIZIALE COSI' CHE SI POSSA INIZIARE AD USARE LA MEMORIA CONDIVISA CON I MECCANISMI
+        //sem_wait e sem_post.
 
         for (i = 1; i < 4; i++) {
-            sem_wait(semW);
-            *p = i;
-            sem_post(semR);
+            sem_wait(semW); //Prendo possesso della memoria condivisa
+            *p = i; //Scrivo sulla memoria condivisa
+            sem_post(semR); //Libero la memoria condivisa
             sleep(2);
         }
 
+        //Ripeto la scrittura ma questa volta metto il valore 99 che quando letto dal child lo fa uscire dal loop e terminare
         sem_wait(semW);
         *p = 99;
         sem_post(semR);
--- ex_sock_cli.c
+++ ex_sock_cli.c
@@ -4,76 +4,81 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <strings.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 
 int
-main(int argc, char **argv)
-{
-int port;
-int protocol;
-char buf[80];
-char ipcli[80];
-int  s, type;			/* Socket descriptor and socket type    */
-int  fd;
-int n;
-struct sockaddr_in cli;		/* Endpoint address of the client  */
+main(int argc, char **argv) {
+    int port;
+    int protocol;
+    char buf[80];
+    char ipcli[80];
+    int s, type;            /* Socket descriptor and socket type    */
+    int fd;
+    int n;
+    struct sockaddr_in cli;        /* Endpoint address of the client  */
 
-	printf("Server IP: ");
-	gets(ipcli);
-	printf("Port to wait for calls: ");
-	gets(buf);
-	port = atoi(buf);
-	if(port < 1024 || port > 65535) {
-		printf("Port number out of range\n");
-		exit(1);
-		}
+    printf("Server IP: ");
+    gets(ipcli); //reads a line from stdin and stores it into the string pointed to by str
+    printf("Port to wait for calls: ");
+    gets(buf); //reads a line from stdin and stores it into the string pointed to by str
+    port = atoi(buf); //Converte da string a intero
+    if (port < 1024 || port > 65535) {
+        printf("Port number out of range\n");
+        exit(1);
+    }
 
 
-	bzero((char *)&cli, sizeof(cli));	/* clear the structure */
-	cli.sin_family = AF_INET;
-	cli.sin_port = htons((u_short)port); 	/* assure the big endianity ! */
+    bzero((char *) &cli, sizeof(cli));    /* clear the structure, svuota il contenuto della struttura cli
+ *  Questo perchè altrimenti trovo dei valori casuali al suo interno, meglio sapere cosa aspettarsi, più che altro se dovessi
+ *  lavorare con dei puntatori si rischia di andare a puntare dei valori fuori dalla memoria che portano a crash*/
+    cli.sin_family = AF_INET; //In pratica ipv4
+    cli.sin_port = htons((u_short) port);    /* Assure the big endianity! perchè nella struttura devono essere memorizzati
+ * in questo formato*/
 
-	if((cli.sin_addr.s_addr = inet_addr(ipcli)) == INADDR_NONE) { //inet_addr converte dalla dotted alla inet
-		printf("Invalid IP address\n");
-		exit(1);
-		}
+    if ((cli.sin_addr.s_addr = inet_addr(ipcli)) ==
+        INADDR_NONE) { //inet_addr converte dalla dotted form (127.99.12.1) alla inet form
+        printf("Invalid IP address\n");
+        exit(1);
+    }
 
-	printf("File to send: "); //Se inserisco "-" apre lo standard input,
-	gets(buf);
-	if(strcmp(buf, "-") == 0) 
-		fd=STDIN_FILENO;
-	else if((fd=open(buf, O_RDONLY)) < 0) {
-		perror(buf);
-		exit(1);
-		}
+    printf("File to send: "); //Se inserisco "-" al posto del path per un file apre lo standard input (vedi if successivo),
+    gets(buf); //Legge input e lo mette in buf
+    if (strcmp(buf, "-") == 0)
+        fd = STDIN_FILENO;
+    else if ((fd = open(buf, O_RDONLY)) <
+             0) { //Apre in lettura il file oppure lo standard input a seconda della scelta precedente
+        perror(buf);
+        exit(1);
+    }
 
-	type=SOCK_STREAM;			/* A TCP socket is desired */
-	protocol = 6;				/* TCP requires 6 */
-	s = socket(PF_INET, type, protocol);
-	if(s < 0) {
-		perror("Can't create socket ");
-		exit(1);
-		}
+    type = SOCK_STREAM;            /* A TCP socket is desired */
+    protocol = 6;                /* TCP requires 6 */
+    s = socket(PF_INET, type, protocol);  //Create the endpoint and locate the file descriptor
+    if (s < 0) {
+        perror("Can't create socket ");
+        exit(1);
+    }
 
-	/* If no bind is called, autobind will be executed by connect sys call */
-	if((connect(s, (struct sockaddr *)&cli, sizeof(cli))) < 0) { //PER il client non serve fare il BIND ma si può fare se si vuole per specificare la porta. Altrimenti lo fa da
-        //solo prendendo una porta casuale come input
-		perror("Connect: ");
-		close(s);
-		exit(1);
-		}
+    /* If no bind is called, autobind will be executed by connect sys call */
+    if ((connect(s, (struct sockaddr *) &cli, sizeof(cli))) < 0) { //PER il client non serve fare il BIND ma
+        //si può fare se si vuole per specificare la porta. Altrimenti lo fa da solo prendendo una porta casuale come input.
+        perror("Connect: ");
+        close(s);
+        exit(1);
+    }
 
-	/* now the server is connected */
-	while((n = read(fd, buf, sizeof(buf))) > 0) {
-		write(s, buf, n);
-		}
+    /* now the server is connected */
+    //Leggo e rimando quello che leggo. (Finchè il fd non viene chiuso?)
+    while ((n = read(fd, buf, sizeof(buf))) > 0) {
+        write(s, buf, n);
+    }
 
-	printf("Client is done\n");
-	close(s);
-	exit(0);
+    printf("Client is done\n");
+    close(s);
+    exit(0);
 }
 
 
